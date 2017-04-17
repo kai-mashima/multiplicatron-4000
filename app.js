3268c38f1953498341fe95deb08ca30b5a84dfa2
Kai Mashima <kaiomashima@gmail.com>
2017-04-17 18:30:22 -0400

    implemented HTML UI

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const multipy = require('./multiplier.js');
 
-console.log(multiply(3, 2));
+class ViewManager {
+
+	connectEventHandlers() {
+		document.getElementById('form-numbers')
+			.addEventListener(
+				'submit', 
+				this.onSubmit.bind(this));
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+
+		const product = multipy(num1, num2);
+
+		this.renderProduct(product);
+	}
+
+	renderProduct (product) {
+		document.querySelector('.mult').textContent = product;
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
