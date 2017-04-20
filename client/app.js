37eca1639b826982b51a0cf23499dc5b24b0ce65
Kai Mashima <kaiomashima@gmail.com>
2017-04-20 14:47:22 -0400

    removal of all var declarations, attempt to use connectEventHandlers function with both calc and new fac

--- client/app.js
+++ client/app.js
@@ -6,37 +6,50 @@ class ViewManager {
 		document.getElementById('form-numbers')
 			.addEventListener(
 				'submit', 
-				this.onSubmit.bind(this));
+				this.calculate.bind(this));
 
-		//document.getElementById('newFactor')
-		//	.addEventListener(
-		//		'submit',
-		//		this.newFactorClick());
+		document.getElementById('newFactor')
+			.addEventListener(
+				'submit',
+				this.newFactor.bind(this));
 	}
-	
-	newFactorClick() {
-		document.getElementById('newFactor').onclick = function () {
-			var target = document.getElementById("newFactors"); //get div that will hold new input fields
-			var newDiv = document.createElement('div'); //create new div to put new input field in
-			var br = document.createElement('br');
-			var newInput = document.createElement('input'); //creat new input field
-			newInput.className = "input-num"; //set class of input field
-			newInput.type = "text"; //set type of input field
-			newInput.autocomplete = "off"; //set autocomplete of input field
-			target.appendChild(newDiv); //add new div to new factor div already on the page
-			target.appendChild(br);
-			newDiv.appendChild(newInput); //add new input to newly added div
 
-		}
-	}
-	
-	onSubmit(event) {
+	newFactor(event) {
 		event.preventDefault();
-		var numbers = []; //array that holds input values
-		var inputFields = document.querySelectorAll('input.input-num'); //grab all the input fields
+		let target = document.getElementById("newFactors"); //get div that will hold new input fields
+		const newDiv = document.createElement('div'); //create new div to put new input field in
+		const br = document.createElement('br');
+		const newInput = document.createElement('input'); //creat new input field
+		newInput.className = "input-num"; //set class of input field
+		newInput.type = "text"; //set type of input field
+		newInput.autocomplete = "off"; //set autocomplete of input field
+		target.appendChild(newDiv); //add new div to new factor div already on the page
+		target.appendChild(br);
+		newDiv.appendChild(newInput); //add new input to newly added div
+	}
+	
+	// newFactorClick() {
+	// 	document.getElementById('newFactor').onclick = function () {
+	// 		let target = document.getElementById("newFactors"); //get div that will hold new input fields
+	// 		const newDiv = document.createElement('div'); //create new div to put new input field in
+	// 		const br = document.createElement('br');
+	// 		const newInput = document.createElement('input'); //creat new input field
+	// 		newInput.className = "input-num"; //set class of input field
+	// 		newInput.type = "text"; //set type of input field
+	// 		newInput.autocomplete = "off"; //set autocomplete of input field
+	// 		target.appendChild(newDiv); //add new div to new factor div already on the page
+	// 		target.appendChild(br);
+	// 		newDiv.appendChild(newInput); //add new input to newly added div
+	// 	}
+	// }
+	
+	calculate(event) {
+		event.preventDefault();
+		let numbers = []; //array that holds input values
+		let inputFields = document.querySelectorAll('input.input-num'); //grab all the input fields
 
-		for (var i = 0; i < inputFields.length; i++) { //for all inputfields 
-			var num = inputFields[i].value;
+		for (let i = 0; i < inputFields.length; i++) { //for all inputfields 
+			let num = inputFields[i].value;
 			if (num != "") { //if input value is empty do not add it to numbers array
 				numbers.push(num); //add valid form input values to array
 			}
@@ -45,7 +58,7 @@ class ViewManager {
 		if (numbers.length == 0 || numbers.length == 1) { //if there are none or only one input field filled out return NaN 
 			document.querySelector('.mult').textContent = "NaN"; //change product div to NaN
 		} else {
-			var total = multiply(numbers); //multiply all the values in the input values array
+			let total = multiply(numbers); //multiply all the values in the input values array
 			this.renderProduct(total);	//call render function to show total
 		}
 	}
@@ -57,4 +70,4 @@ class ViewManager {
 
 const viewManager = new ViewManager();
 viewManager.connectEventHandlers();
-viewManager.newFactorClick();
+// viewManager.newFactorClick();
